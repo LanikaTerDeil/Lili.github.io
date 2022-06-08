f77c09142948f696d5cd54e265bf85a3972cf5be
Lanika <96621531+LanikaTerDeil@users.noreply.github.com>
2022-06-08 20:43:48 +0200

    *

---
+++ js.js
@@ -0,0 +1,253 @@
+/* window.addEventListener('load', () => {
+
+    document.getElementById('video').style.display = 'block';
+    document.getElementById('hi').style.display = 'block'; */
+
+    setTimeout(function(){
+        document.getElementById('hi').style.display = 'none';
+    }, 2000);
+
+    setTimeout(function(){
+        document.getElementById('hi').innerHTML = "Ну-ка, улыбнись)";
+        document.getElementById('hi').style.display = 'block';
+    }, 2003);
+
+    setTimeout(function(){
+        document.getElementById('hi').style.display = 'none';
+    }, 4003);
+
+    setTimeout(function(){
+        document.getElementById('hi').innerHTML = "Я слежу";
+        document.getElementById('hi').style.display = 'block';
+    }, 4006);
+
+    setTimeout(function(){
+        document.getElementById('hi').style.display = 'none';
+    }, 6006);
+
+    setTimeout(function(){
+        document.getElementById('hi').innerHTML = "Улыбнулся?";
+        document.getElementById('hi').style.display = 'block';
+    }, 6009);
+
+    setTimeout(function(){
+        document.getElementById('hi').style.display = 'none';
+    }, 8009);
+
+    setTimeout(function(){
+        document.getElementById('hi').innerHTML = "Молодец";
+        document.getElementById('hi').style.display = 'block';
+    }, 8012);
+
+    setTimeout(function(){
+        document.getElementById('hi').style.display = 'none';
+    }, 10012);
+
+    setTimeout(function(){
+        document.getElementById('hi').innerHTML = "У тебя такая очаровательная улыбка :)";
+        document.getElementById('hi').style.display = 'block';
+    }, 10015);
+
+    setTimeout(function(){
+        document.getElementById('hi').style.display = 'none';
+    }, 13015);
+
+    setTimeout(function(){
+        document.getElementById('hi').innerHTML = "Любой рукастый веб-разработчик запер бы меня в подвале за такой сайтик, </br> поэтому хорошо, что ты не программист... </br> Если что-то будет лагать, извините)";
+        document.getElementById('hi').style.display = 'block';
+    }, 13018);
+
+    setTimeout(function(){
+        document.getElementById('hi').style.display = 'none';
+        document.getElementById('video').style.display = 'none';
+    }, 18015);
+
+    setTimeout(function(){
+        document.getElementById('smoke').style.display = 'block';
+        document.getElementById('dracula').style.display = 'block';
+        document.getElementById('left').style.display = 'block';
+        document.getElementById('right').style.display = 'block';
+        document.getElementsByClassName('full-screen__sidet-content').style.display = 'flex';
+    }, 18018);
+
+    setTimeout(function(){
+        var text = document.createElement('h2');
+        text.innerHTML = "Dracula";
+    }, 18018); 
+
+
+    heart.onclick = function() {
+        document.getElementById('smoke').style.display = 'none';
+        document.getElementById('dracula').style.display = 'none';
+        document.getElementById('left').style.display = 'none';
+        document.getElementById('right').style.display = 'none';
+        document.getElementById('hi').innerHTML = "";
+        document.getElementById('hi').style.display = 'block';
+        document.getElementById('stars').style.display = 'block';
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Не устану говорить, что ты очень мило хмыкаешь и угукаешь)";
+        }, 0000);
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 5003);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Частенько заслушиваюсь, и к глазам подступают слезы от необъятной нежности. “Неужели это чудо и правда сейчас со мной?”";
+            document.getElementById('hi').style.display = 'block';
+        }, 5006)
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 12000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Ты владеешь таким очарованием, что с тобой интересно делать буквально все";
+            document.getElementById('hi').style.display = 'block';
+        }, 12003);
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 17500);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Каждая секунда, проведенная с тобой — счастье в чистом виде";
+            document.getElementById('hi').style.display = 'block';
+        }, 17503)
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 21500+1000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Ты подарил мне то, чего никто не смог";
+            document.getElementById('hi').style.display = 'block';
+        }, 21503+1000);
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 25000+1500);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Умиротворение";
+            document.getElementById('hi').style.display = 'block';
+        }, 25003+1500)
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 28000+3000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Ты успокоил мои мысли, что на протяжении большей части жизни не давали вздохнуть полной грудью и расслабиться";
+            document.getElementById('hi').style.display = 'block';
+        }, 28003+3000)
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 35000+4000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Я почти не говорю о своем состоянии, а если и говорю, то мало, но ты правда вытащил меня из ада";
+            document.getElementById('hi').style.display = 'block';
+        }, 35003+4000);
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 35000+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "И я безмерно тебе благодарна. Ты не представляешь, как много это для меня значит";
+            document.getElementById('hi').style.display = 'block';
+        }, 35003+8000)
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 39000+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Ты, как никто другой, заслуживаешь счастья. Я лишь могу надеяться, что немного этому поспособствую";
+            document.getElementById('hi').style.display = 'block';
+        }, 39003+8000)
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 44000+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Я всегда буду рядышком, что бы не случилось. Обещаю. В любое время дня и ночи";
+            document.getElementById('hi').style.display = 'block';
+        }, 44003+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 49000+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Спасибо за то, что ты есть";
+            document.getElementById('hi').style.display = 'block';
+        }, 49003+8000)
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 51000+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Спасибо, что ты именно такой";
+            document.getElementById('hi').style.display = 'block';
+        }, 51003+8000)
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 55000+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Спасибо за то бессчетное множество улыбок, источником которых ты стал";
+            document.getElementById('hi').style.display = 'block';
+        }, 55003+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 62000+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Ты чудо. Самое настоящее чудо, что так неожиданно пришло в мою жизнь";
+            document.getElementById('hi').style.display = 'block';
+        }, 62003+8000)
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 68000+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Мне не хватит вечности и бесконечности, чтобы выразить свою любовь";
+            document.getElementById('hi').style.display = 'block';
+        }, 68003+8000)
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 74000+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').innerHTML = "Но весь свой короткий человеческий век я потрачу на то, чтобы ты чувствовал себя любимым";
+            document.getElementById('hi').style.display = 'block';
+        }, 74003+8000);
+
+        setTimeout(function(){
+            document.getElementById('hi').style.display = 'none';
+        }, 82000+8000);
+
+        setTimeout(function(){
+            document.getElementById('Kru').style.display = 'block';
+            document.getElementById('stars').style.display = 'none';
+            document.getElementById('hi').innerHTML = "Subride, etenim amatus et in amore es";
+            document.getElementById('hi').style.display = 'block';
+        }, 82003+8000)
+    }
+
+/* }) */
+
+
+
+
+
